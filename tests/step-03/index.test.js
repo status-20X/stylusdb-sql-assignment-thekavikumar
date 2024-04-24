8d810c7d7b5d86488e0fdafd87f2847499b425e2
thekavikumar <kavikumarceo@gmail.com>
2024-04-24 07:51:20 +0530

    step till 8 done

--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -2,7 +2,7 @@ const readCSV = require("../../src/csvReader");
 const parseQuery = require("../../src/queryParser");
 
 test("Read CSV File", async () => {
-  const data = await readCSV("./sample.csv");
+  const data = await readCSV("./student.csv");
   expect(data.length).toBeGreaterThan(0);
   expect(data.length).toBe(3);
   expect(data[0].name).toBe("John");
@@ -10,16 +10,19 @@ test("Read CSV File", async () => {
 });
 
 test("Parse SQL Query", () => {
-  const query = "SELECT id, name FROM sample";
+  const query = "SELECT id, name FROM student";
   const parsed = parseQuery(query);
   expect(parsed).toEqual({
     fields: ["id", "name"],
-    table: "sample",
+    table: "student",
+    joinCondition: null,
+    joinTable: null,
+    whereClauses: [],
   });
 });
 
 // test("Parse Invalid SQL Query", () => {
-//   const invalidQuery = "SELECT id name FROM sample db";
+//   const invalidQuery = "SELECT id name FROM student db";
 //   const parsed = parseQuery(invalidQuery);
 //   expect(parsed).toThrow("Invalid query format");
 // });
